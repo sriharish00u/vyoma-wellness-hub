724bd895a6fac409d8bad73622c2b6928dc970d9
sriharish00u <arishexim011@gmail.com>
2026-05-17 19:59:18 +0530

    404 fix 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
+    tanstackRouter(),
     react(),
     tsconfigPaths(),
+    tailwindcss(),
   ],
 })
